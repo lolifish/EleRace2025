82f648c6ed882eafacf0793e0eaf0e8d8b34fd66
lolifish <lolifish0716@outlook.com>
2025-07-31 15:25:26 +0800

    fix: update project

--- DianSai111/MDK-ARM/DianSai111/stm32h7xx_hal_uart.d
+++ DianSai111/MDK-ARM/DianSai111/stm32h7xx_hal_uart.d
@@ -6,7 +6,7 @@ diansai111\stm32h7xx_hal_uart.o: ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_h
 diansai111\stm32h7xx_hal_uart.o: ../Drivers/CMSIS/Device/ST/STM32H7xx/Include/stm32h7xx.h
 diansai111\stm32h7xx_hal_uart.o: ../Drivers/CMSIS/Device/ST/STM32H7xx/Include/stm32h750xx.h
 diansai111\stm32h7xx_hal_uart.o: ../Drivers/CMSIS/Include/core_cm7.h
-diansai111\stm32h7xx_hal_uart.o: D:\Keil5\ARM\ARMCC\Bin\..\include\stdint.h
+diansai111\stm32h7xx_hal_uart.o: D:\mcu\keil5_core\ARM\ARMCC\Bin\..\include\stdint.h
 diansai111\stm32h7xx_hal_uart.o: ../Drivers/CMSIS/Include/cmsis_version.h
 diansai111\stm32h7xx_hal_uart.o: ../Drivers/CMSIS/Include/cmsis_compiler.h
 diansai111\stm32h7xx_hal_uart.o: ../Drivers/CMSIS/Include/cmsis_armcc.h
@@ -14,8 +14,8 @@ diansai111\stm32h7xx_hal_uart.o: ../Drivers/CMSIS/Include/mpu_armv7.h
 diansai111\stm32h7xx_hal_uart.o: ../Drivers/CMSIS/Device/ST/STM32H7xx/Include/system_stm32h7xx.h
 diansai111\stm32h7xx_hal_uart.o: ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal.h
 diansai111\stm32h7xx_hal_uart.o: ../Drivers/STM32H7xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h
-diansai111\stm32h7xx_hal_uart.o: D:\Keil5\ARM\ARMCC\Bin\..\include\stddef.h
-diansai111\stm32h7xx_hal_uart.o: D:\Keil5\ARM\ARMCC\Bin\..\include\math.h
+diansai111\stm32h7xx_hal_uart.o: D:\mcu\keil5_core\ARM\ARMCC\Bin\..\include\stddef.h
+diansai111\stm32h7xx_hal_uart.o: D:\mcu\keil5_core\ARM\ARMCC\Bin\..\include\math.h
 diansai111\stm32h7xx_hal_uart.o: ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_rcc_ex.h
 diansai111\stm32h7xx_hal_uart.o: ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_gpio.h
 diansai111\stm32h7xx_hal_uart.o: ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_gpio_ex.h
